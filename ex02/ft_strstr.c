f8d8ac40c895e18d147fafb1ed667aae9772bad0
vmokhomo <67002083+vmokhomo@users.noreply.github.com>
2020-06-24 13:39:24 +0200

    Update ft_strstr.c

--- ex02/ft_strstr.c
+++ ex02/ft_strstr.c
@@ -10,9 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-/* void ft_putchar(char c)
+/*	#include <unistd.h>
+**
+** 	void ft_putchar(char c)
 **	{
 **		write(1, &c, 1);
 **	}
